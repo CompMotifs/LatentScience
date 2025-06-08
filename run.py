dba5ffc980f0a48419764bc228d8ac5ad4fe69ac
olive004 <olivia.gallup@gmail.com>
2025-06-08 11:44:09 +0100

    simplify

--- run.py
+++ run.py
@@ -11,20 +11,20 @@ def find_comparison(abstract, research_question):
     
     # Generate embedding for abstract
     embedder = PaperEmbedder(model_name="all-MiniLM-L6-v2")
-    embedding_just_abstract = embedder.get_embedding(abstract)
+    # embedding_just_abstract = embedder.get_embedding(abstract)
     embedding_simp_abstract = embedder.get_embedding(prompt_rephrase)
     
     # Combine the embeddings of the raw and rephrased abstracts
-    embedding_mixed = embedding_just_abstract + embedding_simp_abstract
+    # embedding_mixed = embedding_just_abstract + embedding_simp_abstract
     
     # Compare embedding with database embeddings
     big_number = int(1e6)
     similarifier = SimilarityService(top_choices=big_number, dim=-1)
     
-    if use_mixed:
-        top_picks, top_picks_idx = similarifier(embedding_mixed, database)
-    else:
-        top_picks, top_picks_idx = similarifier(embedding_simp_abstract, database)
+    # if use_mixed:
+    #     top_picks, top_picks_idx = similarifier(embedding_mixed, database)
+    # else:
+    top_picks, top_picks_idx = similarifier(embedding_simp_abstract, database)
 
     # Return the top picks
     return top_picks, top_picks_idx
